daad9a9c0edcdf94eac52323b0ef849f1a954704
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-06-03 13:49:28 +0530

    Add files via upload

---
+++ assignments/input_output_assignments-2.py
@@ -0,0 +1,18 @@
+"""
+Write a Python program that asks the user to enter their name using the input() function, and then prints a greeting message using the print() function. For example, if the user enters "Alice", the program should print "Hello, Alice!"
+"""
+
+"""
+WAP to print the following output in single of code
+'Hello' : dynamic using input()
+'Anil' : dynamic using input()
+Note: please use print() to combine the output
+Final output format:
+Hello Anil
+Hi Anil
+I Love Python
+"""
+
+"""
+Write a script that asks the user for their name, age, and favorite color, and then prints a single sentence summarizing this information in a formatted string.
+"""
