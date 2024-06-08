feecfbc1565a7b06572940d4bac3884fcad8d437
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-06-08 18:15:21 +0530

    Add files via upload

---
+++ assignments/strings-assignment.py
@@ -0,0 +1,13 @@
+"""
+Write a Program to retrieve character 'X' using positive indexing
+from "Semantic segmentation of Lung Cancer from Chest X-Ray" string
+"""
+
+'''
+Write a Program to retrieve the 'L' character from "I have to Love Python Programming" string
+using negative/reverse indexing
+'''
+
+'''
+Can prove that python string is immutable with the program
+'''
