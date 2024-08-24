74f1c780ea2540720bb86499ed8d3eaf2906362d
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-08-24 21:59:48 +0530

    Add files via upload

---
+++ assignments/functions/function_assignments.py
@@ -0,0 +1,10 @@
+"""
+1. Array and String Manipulation
+Question: Given an array of integers, write a function that
+returns the array with all zeros moved to the end while
+maintaining the relative order of the non-zero elements.
+input: [2, 0, 1, 34, 0, 900]
+output: [2, 1, 34, 900, 0, 0]
+"""
+
+# write a Python program to find the fibonacci series using recursive and non recursive functions
