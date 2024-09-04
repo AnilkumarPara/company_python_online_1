64147914763cb9e10ed3394d5beda71e019fdc77
Anilkumar Para <50998167+AnilkumarPara@users.noreply.github.com>
2024-09-04 11:26:04 +0530

    Add files via upload

---
+++ assignments/advanced_python/OOPs/oops.py
@@ -0,0 +1,10 @@
+"""
+1. Create a Bank class which demonstrates the class, instance variables
+and also the instance, class and static methods
+"""
+
+"""
+2. Create a Vehicle class as Parent and choose any Vehicle (example: car) as Child class,
+which should demonstrates the single inheritance
+"""
+
